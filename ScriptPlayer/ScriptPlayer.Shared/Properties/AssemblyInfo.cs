a4d1400545e62b3e47b5c4433aeab24524b48899
Fred Tungsten <f_tungsten@outlook.com>
2017-07-23 21:59:51 +0200

    AssemblyInfo.cs

--- ScriptPlayer/ScriptPlayer.Shared/Properties/AssemblyInfo.cs
+++ ScriptPlayer/ScriptPlayer.Shared/Properties/AssemblyInfo.cs
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+// Use informational version to store git hashes
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
--- ScriptPlayer/ScriptPlayer.VideoSync/Properties/AssemblyInfo.cs
+++ ScriptPlayer/ScriptPlayer.VideoSync/Properties/AssemblyInfo.cs
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+// Use informational version to store git hashes
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
