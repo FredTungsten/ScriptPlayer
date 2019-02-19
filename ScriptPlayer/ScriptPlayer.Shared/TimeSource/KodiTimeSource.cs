0f95ce89f3963cd3a5f75e6645ae624aa0177f80
gagax123 <gagax123@web.de>
2019-02-19 12:29:04 +0100

    smb paths for windows

--- ScriptPlayer/ScriptPlayer.Shared/TimeSource/KodiTimeSource.cs
+++ ScriptPlayer/ScriptPlayer.Shared/TimeSource/KodiTimeSource.cs
@@ -10,6 +10,7 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Net;
+using System.Text.RegularExpressions;
 
 /*
  * Done: Handle when a video is resumed: tested although not always the best accuracy
@@ -47,7 +48,7 @@ namespace ScriptPlayer.Shared
         private readonly ManualTimeSource _timeSource;
 
         private ClientWebSocket _websocket;
-        private readonly CancellationTokenSource _cts;
+        private readonly CancellationTokenSource _cts; // I have no idea how this thing works
 
         private bool _running = true;
 
@@ -71,9 +72,9 @@ namespace ScriptPlayer.Shared
             task.Wait();
         }
 
-        private static async Task<string> ReadString(ClientWebSocket ws)
+        private static async Task<string> ReadString(ClientWebSocket ws, CancellationToken ct)
         {
-            ArraySegment<Byte> buffer = new ArraySegment<byte>(new Byte[65536]);
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[65536]);
 
             WebSocketReceiveResult result = null;
 
@@ -81,7 +82,7 @@ namespace ScriptPlayer.Shared
             {
                 do
                 {
-                    result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    result = await ws.ReceiveAsync(buffer, ct);
                     ms.Write(buffer.Array, buffer.Offset, result.Count);
                 }
                 while (!result.EndOfMessage);
@@ -95,7 +96,7 @@ namespace ScriptPlayer.Shared
 
         private static string ReadStringSync(ClientWebSocket ws, CancellationToken ct)
         {
-            var task = ReadString(ws);
+            var task = ReadString(ws, ct);
             try
             {
                 task.Wait(ct);
@@ -135,10 +136,19 @@ namespace ScriptPlayer.Shared
                 string filepath = json_data_obj["result"]["item"]["file"]?.ToString();             
 
                 // make smb path windows compatible
-                if(filepath.StartsWith("smb:"))
+                if (filepath.StartsWith("smb:"))
                 {
                     filepath = filepath.Replace("smb:", "");
                     filepath = filepath.Replace('/', '\\');
+                    // under some circumstances kodi seems to put user and password in the filepath aswell
+                    // smb://user:pass@server/share which doesn't work with windows so we try to remove "user:password@"
+                    if (filepath.Contains('@'))
+                    {
+                        filepath = Regex.Replace(filepath, @"(?<=\\\\).*:.*@", ""); // I have no idea if this regular expression is robust enough 
+                                                                                    // to only remove "user:password@" all the time
+                                                                                    // we are also assuming filepath starts with \\
+                    }
+
                 }
                 Console.WriteLine("currently playing: " + filepath);
 
@@ -452,7 +462,12 @@ namespace ScriptPlayer.Shared
                         SetConnected(true);
                     }
                     Console.WriteLine("msg: " + result);
-                    handle_msg(result);
+                    try
+                    {
+                        handle_msg(result);
+                    }
+                    catch(Exception e) { }
+                    
                 }
             }
 
@@ -632,6 +647,9 @@ namespace ScriptPlayer.Shared
         public void UpdateConnectionSettings(KodiConnectionSettings settings)
         {
             _connectionSettings = settings;
+            // if settings are being updated while connected
+            // the websocket connection will not get updated
+            // could just restart the Thread here if it's running
         }
     }
 }
