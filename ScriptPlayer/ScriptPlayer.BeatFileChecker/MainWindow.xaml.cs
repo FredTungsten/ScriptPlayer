26e8015e6ecb548375d7e04854a84f88d9c00ceb
Fred Tungsten <f_tungsten@outlook.com>
2019-06-29 11:05:51 +0200

    Changed BeatFileChecker to analyse BPM

--- ScriptPlayer/ScriptPlayer.BeatFileChecker/MainWindow.xaml.cs
+++ ScriptPlayer/ScriptPlayer.BeatFileChecker/MainWindow.xaml.cs
@@ -35,38 +35,28 @@ namespace ScriptPlayer.BeatFileChecker
 
             string[] files = paths.SelectMany(path => Directory.EnumerateFiles(path, "*.txt", SearchOption.AllDirectories)).ToArray();
 
-            List<Tuple<string, int>> brokenFiles = new List<Tuple<string, int>>();
+            List<ScriptStats> stats = new List<ScriptStats>();
 
             foreach (string textFile in files)
             {
                 try
                 {
-                    TimeSpan lastbeat = TimeSpan.Zero;
-                    int beatcount = 0;
+                    BeatCollection collection = BeatCollection.Load(textFile);
 
-                    using (FileStream stream = new FileStream(textFile, FileMode.Open, FileAccess.Read))
+                    var chapters = GetChapters(collection);
+                    if (chapters == null || chapters.Count < 1)
+                        continue;
+
+                    TimeSpan duration = chapters.Aggregate(TimeSpan.Zero, (total, current) => total + (current.Last() - current.First()));
+                    double bpm = chapters.Sum(c => c.Count - 1) / duration.TotalMinutes;
+
+                    stats.Add(new ScriptStats
                     {
-                        using (var reader = new StreamReader(stream))
-                        {
-                            while (!reader.EndOfStream)
-                            {
-                                string line = reader.ReadLine();
-                                if (string.IsNullOrWhiteSpace(line))
-                                    continue;
-
-                                TimeSpan beat = TimeSpan.FromSeconds(double.Parse(line.Replace(",", "."), CultureInfo.InvariantCulture));
-                                beatcount++;
-
-                                if (lastbeat > beat)
-                                {
-                                    brokenFiles.Add(new Tuple<string, int>(textFile, beatcount));
-                                    break;
-                                }
-
-                                lastbeat = beat;
-                            }
-                        }
-                    }
+                        Beats = collection.ToList(),
+                        Bpm = bpm,
+                        ContentDuration = duration,
+                        File = textFile
+                    });
                 }
                 catch
                 {
@@ -74,8 +64,67 @@ namespace ScriptPlayer.BeatFileChecker
                 }
             }
 
-            foreach(var tup in brokenFiles)
-                Debug.WriteLine($"{tup.Item2:######} | {tup.Item1}");
+            stats = stats.OrderByDescending(s => s.Bpm).ToList();
+
+            int pos = 0;
+            foreach (var stat in stats)
+            {
+                pos++;
+                Debug.WriteLine($"{pos.ToString().PadRight(2)}.: {stat.Bpm:f0} BMP, {stat.ContentDuration:h\\:mm\\:ss}, {System.IO.Path.GetFileNameWithoutExtension(stat.File)}");
+            }
         }
+
+        private List<List<TimeSpan>> GetChapters(BeatCollection timeStamps)
+        {
+            TimeSpan gapDuration = TimeSpan.FromSeconds(10);
+
+            if (timeStamps.Count < 2)
+                return null;
+
+            int chapterBegin = int.MinValue;
+            int chapterEnd = int.MinValue;
+
+            List<List<TimeSpan>> chapters = new List<List<TimeSpan>>();
+
+            for (int index = 0; index < timeStamps.Count; index++)
+            {
+                if (chapterBegin == int.MinValue)
+                {
+                    chapterBegin = index;
+                    chapterEnd = index;
+                }
+                else if (timeStamps[index] - timeStamps[chapterEnd] < gapDuration)
+                {
+                    chapterEnd = index;
+                }
+                else
+                {
+                    chapters.Add(GetRange(timeStamps,chapterBegin, chapterEnd));
+
+                    chapterBegin = index;
+                    chapterEnd = index;
+                }
+            }
+
+            if (chapterBegin != int.MinValue && chapterEnd != int.MinValue)
+            {
+                chapters.Add(GetRange(timeStamps, chapterBegin, chapterEnd));
+            }
+
+            return chapters;
+        }
+
+        private List<T> GetRange<T>(IEnumerable<T> source, int firstIndex, int lastIndex)
+        {
+            return source.Skip(firstIndex).Take(lastIndex - firstIndex + 1).ToList();
+        }
+    }
+
+    public class ScriptStats
+    {
+        public double Bpm { get; set; }
+        public List<TimeSpan> Beats { get; set; }
+        public TimeSpan ContentDuration { get; set; }
+        public string File { get; set; }
     }
 }
